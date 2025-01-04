f204e8905e28bdd007f1cc96f4fd24c0074d724d
ahmed hosny <ahmed.hosny@the-diy-life.co>
2025-01-04 16:49:44 +0200

    update

--- js/modal_script.js
+++ js/modal_script.js
@@ -6,9 +6,35 @@ const projects = [
         keywords: ["Raspberry Pi", "Embedded Linux", "PyQt", "Python", "MySQL", "Automation"]
     },
     {
-        title: "Project 2",
-        description: "This is a detailed description of Project 2.",
-        images: ["project2.jpg"]
+        title: "SMARTZEE PLATFORM",
+        description: " SMARTZEE is A cloud platform that gathers and monitor data from smart meters .<br><strong>Features</strong><br> <ul> <li> Add, monitor and control SmartZee devices from discrete locations from the cloud.</li><li>Define child smart meters under each ZEE device, monitor PV system power/energy readings and control energy import/export.</li>\
+<li>Add discrete standalone smart meters and monitor their readings from the cloud.</li>\
+<li>Remotely control system’s energy import/export from full export to zero export ( feed-in to grid ) based on power setpoint, time schedule, voltage , or simply switch to local config with a single button click.</li></ul>",
+        // images: ["img/projects/Untitled.png","img/projects/Untitled1.png","img/projects/Untitled2.png","img/projects/Untitled3.png","img/projects/Untitled4.png","img/projects/slider-phone.png"]
+        images: ["img/projects/Untitled.png", "img/projects/Untitled1.png", "img/projects/Untitled2.png", "img/projects/Untitled3.png", "img/projects/Untitled4.png", "img/projects/slider-phone.png"],
+        keywords: ["Django", "Django REST framework", "DEVOPS", "CIRCLECI", "Docker", "AWS IOT", "AWS Lambda" , "AWS RDS", "AWS CloudFormation", "WebSocket"]
+    },
+    {
+        title: "IOT SMART METER",
+        description: `<p style="text-align: left;"> <strong>My role: </strong>implement all features and protocols of power meter (IOT,SPI,modbus,aws IOT,MQTT,internal webserver)<br>\
+<strong>Project description:</strong> elgris Smart Meter is an innovative measuring device and enables consumers to monitor and control their electricity consumption. It records the feed-in and consumption values ​​with phase accuracy and in real time.\
+<br><strong>feature:</strong><br>\
+<ul style="text-align: left;"><li>Control of Shelly products (smart plugs)</li>\
+<li>Functionally compatible with SMA products</li>\
+<li>SUNSPEC MODBUS/TCP server integrated (LAN)</li>\
+<li>Real-time measurement of consumption and feed-in with up to 50 ms (50 Hz) update</li>\
+<li>Easy operation via integrated web configuration</li>\
+<li>Multifunctional: Simultaneous communication with up to 4 different inverters / brands</li></ul></p>`,
+        // images: ["img/projects/Untitled.png","img/projects/Untitled1.png","img/projects/Untitled2.png","img/projects/Untitled3.png","img/projects/Untitled4.png","img/projects/slider-phone.png"]
+        images: ["img/projects/sm_lan_2.0_final_GIF_v1.1.gif", "img/projects/SM-IOT-Diagram-2024-1024x419.jpg"],
+        keywords: ["ESP32", "ESP-IDF", "IOT", "MQTT", "MODBUS", "SPI", "Mdns", "AWS IOT Core"]
+    },
+    {
+        title: "IOT with industrial boiler",
+        description: `<p style="text-align: left;"> <strong>Project description:</strong> The goal of project is to control the process of industrial boiler and publish its reading to AWS using esp32 controller and idf framework based on AWS IOT Core for fleet provisioning , registration , collecting data and store in AWS dynamoDB, sending commands for settings and Over The Air update (OTA)</p>`,
+        // images: ["img/projects/Untitled.png","img/projects/Untitled1.png","img/projects/Untitled2.png","img/projects/Untitled3.png","img/projects/Untitled4.png","img/projects/slider-phone.png"]
+        images: ["img/projects/Industrial_Boiler_Control_Project.jpg"],
+        keywords: ["AWS IoT Core", "Arduino", "ESP32", "Fleet Provisioning"]
     },
     // Add more projects as needed
 ];
@@ -26,7 +52,8 @@ function showModal(index) {
     // Set modal content
     modal.style.display = "flex";
     modalTitle.textContent = projects[index].title;
-    modalDescription.textContent = projects[index].description;
+    //modalDescription.textContent = projects[index].description;
+    modalDescription.innerHTML = projects[index].description;
 
     // Clear previous images
     modalImagesContainer.innerHTML = "";
@@ -36,12 +63,17 @@ function showModal(index) {
         const img = document.createElement("img");
         img.src = imageSrc;
         img.style.display = idx === 0 ? "block" : "none"; // Show only the first image
+        img.style.marginTop = "300px";
+        img.style.height = "200px";
         modalImagesContainer.appendChild(img);
     });
 
     keywordsList.innerHTML = ""; // Clear previous keywords
     projects[index].keywords.forEach((keyword) => {
-        const li = document.createElement("li");
+        // const li = document.createElement("li");
+        // li.textContent = keyword;
+        // keywordsList.appendChild(li);
+        const li = document.createElement("button");
         li.textContent = keyword;
         keywordsList.appendChild(li);
     });
